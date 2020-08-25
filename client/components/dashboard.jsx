e0cb62f1b05ac6ea715acf8173f02c7b34ab5e84
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2020-08-25 18:44:08 -0400

    feat: remove graphql

--- client/components/dashboard.jsx
+++ client/components/dashboard.jsx
@@ -1,33 +1,3 @@
 import React from "react";
-import { Query } from "react-apollo";
-import { gql } from "apollo-boost";
 
-export const Dashboard = () => (
-  <Query
-    query={gql`
-      {
-        me {
-          id
-          name
-          email
-        }
-      }
-    `}
-  >
-    {({ loading, error, data }) => {
-      if (loading) return <p>Loading...</p>;
-      if (error) return <p>Error :(</p>;
-
-      return (
-        <>
-          <h1>You are:</h1>
-          <ul>
-            <li>id: {data.me.id}</li>
-            <li>name: {data.me.name}</li>
-            <li>email: {data.me.email}</li>
-          </ul>
-        </>
-      );
-    }}
-  </Query>
-);
+export const Dashboard = () => <h1>Welcome to the Dashboard</h1>;
--- client/index.jsx
+++ client/index.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-import { ApolloProvider } from "react-apollo";
 
-import { client } from "./lib/apollo";
 import { App } from "./app";
 
 const wrapper = document.getElementById("root");
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <Router>
-      <App />
-    </Router>
-  </ApolloProvider>,
+  <Router>
+    <App />
+  </Router>,
   wrapper
 );
